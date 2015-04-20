73f83075cba0f6629801e38ac66a88b01f26bbe7
tantecky <tantecky@seznam.cz>
2015-04-20 12:19:16 +0200

    added script for bootstrapping vim

---
+++ vim/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -u
+set -e
+
+REPO='https://github.com/tantecky/myscripts.git'
+SCRIPT_DIR="~/myscripts"
+eval SCRIPT_DIR=$SCRIPT_DIR
+VIM_DIR="$SCRIPT_DIR/vim"
+
+if [[ -e $SCRIPT_DIR ]]; then
+  cd $VIM_DIR
+  git pull
+else
+  cd $SCRIPT_DIR
+  git clone $REPO
+fi
+
+cd $VIM_DIR
+
+./update_plugins.sh
+rm -rf ~/.vimrc
+rm -rf ~/.vim
+cd ~
+ln -s "$SCRIPT_DIR/vim" .vim
+ln -s "$SCRIPT_DIR/vim/.vimrc" .vimrc
